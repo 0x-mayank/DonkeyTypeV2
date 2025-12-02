1324f62f0207573b2978b2ebb58e27009f509c67
0x-mayank <mayankm1946@gmail.com>
2025-12-02 15:51:18 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Leaderboard from './pages/Leaderboard'
+import Login from './pages/Login'
+import Test from './pages/Test'
+
 
 function App() {
-
+  
   return (
-    <>
-      
-    </>
+    <div className='min-h-screen bg-[#f2f0e3] text-gray-800'>
+      <Navbar/>
+      <main className='max-w-9xl mx-auto px-4 py-8'>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/test" element={<Test/>} />
+          <Route path='/leaderboard' element={<Leaderboard/>} />
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </main>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import {Home, Trophy, User, LogOut, Zap} from 'lucide-react'
+import Logo from '../assets/logo.svg'
+
+
+export default function Navbar() {
+  return (
+    <div className='w-full flex justify-center py-6 font-display'>
+        <nav className='flex items-center gap-2 rounded-xl px-5 py-2'>
+            <div className='flex items-center gap-1 mr-3'>
+                <img src={Logo} alt="logo"
+                className='w-7 h-auto mb-1'
+                />
+
+                <span className='font-semibold text-gray-800 text-m tracking-wide select-none'>
+                    donkeyType
+                </span>
+            </div>
+
+            <NavLink
+            to='/'
+            className={({isActive})=> 
+                `flex items-center gap-1.5 px-3 py-1.5 rounded-md text-m font-medium transition-colors ${
+                    isActive? "bg-gray-700 text-white" : 'text-gray-800 hover:bg-gray-200'
+                }`
+            }
+            >
+            <Home size={15} strokeWidth={2} /> Home
+            </NavLink>
+
+            <NavLink
+            to='/test'
+            className={({isActive})=> 
+                `flex items-center gap-1.5 px-3 py-1.5 rounded-md text-m font-medium transition-colors ${
+                    isActive? "bg-gray-700 text-white" : 'text-gray-800 hover:bg-gray-200'
+                }`
+            }
+            >
+            <Zap size={15} strokeWidth={2}/>
+                Practice
+            </NavLink>
+
+            <NavLink
+            to='/leaderboard'
+            className={({isActive})=> 
+                `flex items-center gap-1.5 px-3 py-1.5 rounded-md text-m font-medium transition-colors ${
+                    isActive? "bg-gray-700 text-white" : 'text-gray-800 hover:bg-gray-200'
+                }`
+            }
+            >
+            <Trophy size={15} strokeWidth={2} />
+                Leaderboard
+            </NavLink>
+
+            <div className='h-6 w-0.5 bg-gray-700 mx-2'></div>
+
+            <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              `flex items-center gap-1.5 px-2 py-1.5 rounded-md text-m font-medium ${
+                isActive? "bg-gray-700 text-white": "text-gray-800 hover:bg-gray-200"
+              }`
+            }
+          >
+            <User size={15} strokeWidth={2} />
+            SignUp
+          </NavLink>
+        </nav>
+
+    </div>
+  )
+}
