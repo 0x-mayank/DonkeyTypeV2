c09f0feb309f8810ebf993621155f2668288afa2
0x-mayank <mayankm1946@gmail.com>
2025-12-16 16:09:06 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
@@ -8,6 +8,28 @@ import Test from './pages/Test'
 import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth < 1024);
+    };
+
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
+
+    return () => window.removeEventListener('resize', checkScreenSize);
+  }, []);
+  if (isMobile) {
+    return (
+      <div className='min-h-screen bg-[#f2f0e3] flex flex-col justify-center items-center p-6 text-center'>
+        <h1 className='font-instrument text-2xl md:text-3xl text-gray-800 mb-4'>
+          <span className='text-[#f76f53] italic'>donkeyType</span> is only available on desktop
+        </h1>
+      </div>
+    );
+  }
+
   return (
     <div className='min-h-screen bg-[#f2f0e3] text-gray-800'>
       <Navbar/>
